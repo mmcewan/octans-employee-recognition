315e8f399955714f7d477ce4b35cdc8e4466c4c6
Mark McEwan <mark.a.mcewan@gmail.com>
2017-07-13 23:10:28 -0400

    added custom password validation, renamed js file to octans

---
+++ public/js/octans.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+
+  $('#signup-button').click(function() {
+    event.preventDefault();
+    $.get("/signup", function(data) {
+      $("#content").html(data);
+    });
+  });
+
+  $('#login-button').click(function() {
+    event.preventDefault();
+    $.get("/login", function(data) {
+      $("#content").html(data);
+    });
+  });
+
+  $('#password').change(validatePasword);
+  $('#confirm-password').change(validatePasword);
+
+});
+
+// custom validation to ensure password and confirm password fields match
+function validatePasword() {
+  var p1 = $('#password').val();
+  var p2 = $('#confirm-password').val();
+  if (p1 != p2) {
+    $('#confirm-password').get(0).setCustomValidity("Passwords do not match.");
+  }
+  else {
+    $('#confirm-password').get(0).setCustomValidity('');
+  }
+}
