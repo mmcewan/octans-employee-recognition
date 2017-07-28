b5be6c97c7eebe5f9f0d5a14246efb0c2af08b84
francise <francise@oregonstate.edu>
2017-07-28 19:24:55 +0900

    updated createward route

--- public/js/createaward.js
+++ public/js/createaward.js
@@ -14,10 +14,25 @@
           payload.atype = document.getElementById('alist').value;
           
           req.open('POST', '/new_award', true);
+          req.responseType = "blob";
           req.setRequestHeader('Content-Type', 'application/json');
           req.addEventListener('load', function(){
-                var response = req.responseText;
-                console.log(response);});
+          		if (this.status === 200) {
+          		// `blob` response
+          		console.log(this.response);
+          		// create `objectURL` of `this.response` : `.pdf` as `Blob`
+          		var file = window.URL.createObjectURL(this.response);
+          		var a = document.createElement("a");
+          		a.href = file;
+          		a.download = this.response.name || "detailPDF";
+          		document.body.appendChild(a);
+          		a.click();
+          		// remove `a` following `Save As` dialog, 
+          		// `window` regains `focus`
+          		window.onfocus = function () {                     
+          		document.body.removeChild(a)
+        		}
+        	};});
           req.send(JSON.stringify(payload));  
         }); 
       }
